f1375c19e0641ebdcbbe0900ecfd225edd174a68
mdkhaledbin <mdkhaledbin1221@gmail.com>
2024-03-02 12:00:29 +0600

    First

---
+++ app/src/main/java/com/example/eateryapp/Data/GlobalVariable.kt
@@ -0,0 +1,21 @@
+package com.example.eateryapp.Data
+
+import androidx.compose.runtime.mutableStateListOf
+import com.google.firebase.database.FirebaseDatabase
+
+
+var totalItemInCart:Int=0
+var selectedResID = -1
+
+
+var resName =  mutableStateListOf<RestaurantName>()
+var itm : List<RestaurantItems> = emptyList()
+
+
+//database section
+var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+var totalUser:Int=0
+var currentUserId:Int = 0
+
+var ok : Boolean = false
